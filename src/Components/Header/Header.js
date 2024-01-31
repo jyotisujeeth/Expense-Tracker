2cb1c386216885ed863cb3d3c984f0eb45abd2f0
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2024-01-31 13:02:45 +0530

    added logOut feature

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,34 @@
 import React from 'react'
 
-import { Navbar, Nav, Container, } from 'react-bootstrap';
-import { Link, } from 'react-router-dom';
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Link,useHistory } from 'react-router-dom';
 
-const Header = () => {
+const Header = () => {const history = useHistory();
+  const logOutHandler = () => {
+    history.replace('/login')
+    localStorage.removeItem('token');
+    localStorage.removeItem('email');
+  }
+
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        
         <Navbar.Brand>
-          <Link to="/home" style={{color:"white",textDecoration:"none"}}>Expense-Tracker</Link>
+           <Link to="/home" style={{ color: "white", textDecoration: "none" }}>
+            Expense-Tracker
+          </Link>
         </Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Products</Nav.Link>
+          <Nav.Link href="#pricing">AboutUs</Nav.Link>
+           <Button onClick={logOutHandler} variant="light" size="sm">
+            Logout
+          </Button>
+        </Nav>
+      </Container>
+    </Navbar>
+)
+}
+
+export default Header;
